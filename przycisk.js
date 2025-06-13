68db3f3d662765dafabd7a138ce98753a2750dc0
xMKWYx <138711719+xMKWYx@users.noreply.github.com>
2025-06-13 22:39:26 +0200

    Add files via upload

---
+++ przycisk.js
@@ -0,0 +1,9 @@
+function powiadomieniedawidowe() {
+  const password = prompt("Podaj hasło:");
+
+  if (password === "banan") {
+    alert("NASTĘPNYM RAZEM ODDA!");
+  } else {
+    alert("Nieprawidłowe hasło!");
+  }
+}
